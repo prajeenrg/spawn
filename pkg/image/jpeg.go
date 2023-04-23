0fb2473b8b77f92de47ba6bb26b25bde2f929d99
Prajeen Govardhanam <prajeenrg@gmail.com>
2023-04-23 18:08:26 +0530

    fix: lint errors

--- pkg/image/jpeg.go
+++ pkg/image/jpeg.go
@@ -26,7 +26,7 @@ func MakeJpegs(directory, prefix string, d *Dimens, count uint) {
 	util.CreateFolderIfNotExits(directory)
 	bar := util.GetProgressBar(count, "Generating JPEG files")
 	for i := uint(1); i <= count; i++ {
-		bar.Add(1)
+		util.Increment(&bar)
 		filename := fmt.Sprintf("%s/%s_%dx%d_%d.jpg", directory, prefix, d.Width, d.Height, i)
 		image := GenerateImage(d)
 		MakeJpeg(filename, image)
--- pkg/util/progress.go
+++ pkg/util/progress.go
@@ -1,7 +1,17 @@
 package util
 
-import pb "github.com/schollz/progressbar/v3"
+import (
+	"log"
+
+	pb "github.com/schollz/progressbar/v3"
+)
 
 func GetProgressBar(count uint, desp string) pb.ProgressBar {
 	return *pb.Default(int64(count), desp)
+}
+
+func Increment(bar *pb.ProgressBar) {
+	if err := bar.Add(1); err != nil {
+		log.Println("Progress bar update failed")
+	}
 }
