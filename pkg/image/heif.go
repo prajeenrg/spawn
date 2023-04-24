ae6738cae5fdb18f9a1a1b3daa7c4e6e6d66f2bc
Prajeen Govardhanam <prajeenrg@gmail.com>
2023-04-24 22:13:37 +0530

    add: heic support func

---
+++ pkg/image/heif.go
@@ -0,0 +1,38 @@
+package image
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/carck/libheif/go/heif"
+	"github.com/prajeenrg/spawn/pkg/util"
+)
+
+type HeifGenerator struct{}
+
+func (h *HeifGenerator) SingleImage(name string, d *Dimens) {
+	image := generateImage(d)
+	ctx, err := heif.EncodeFromImage(image, heif.CompressionHEVC, 100, heif.LosslessModeEnabled, heif.LoggingLevelFull)
+
+	if err != nil {
+		log.Fatalln("Cannot create heif file from image")
+	}
+
+	if !util.CheckExtension(name, "heic") {
+		name = fmt.Sprintf("%s.heic", name)
+	}
+
+	if err := ctx.WriteToFile(name); err != nil {
+		log.Fatalln("Failed to write heif image")
+	}
+}
+
+func (h *HeifGenerator) MultipleImages(directory, prefix string, d *Dimens, count uint) {
+	util.CreateFolderIfNotExits(directory)
+	bar := util.GetProgressBar(count, "Generating WebP files")
+	for i := uint(1); i <= count; i++ {
+		filename := fmt.Sprintf("%s/%s_%dx%d_%d.heic", directory, prefix, d.Width, d.Height, i)
+		h.SingleImage(filename, d)
+		util.Increment(&bar)
+	}
+}
